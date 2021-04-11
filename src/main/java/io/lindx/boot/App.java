a5e6520d0cbede952b18177c05cb7b63b323e14b
lindx <linder.igor@gmail.com>
2021-04-12 00:37:37 +0300

    SpringApplication.run

--- src/main/java/io/lindx/boot/App.java
+++ src/main/java/io/lindx/boot/App.java
@@ -1,6 +1,4 @@
 package io.lindx.boot;
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
   public static void main(String[] args) {
 
-    SpringApplication app = new SpringApplication(App.class);
-      app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
-      app.run(args);
+    SpringApplication.run(App.class, args);
   }
 }
