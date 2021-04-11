3e1dd9eb7f22f458e22e19d3a48a1ac425dc908d
lindx <linder.igor@gmail.com>
2021-04-12 00:42:27 +0300

    RestTemplateBuilder

--- src/main/java/io/lindx/boot/App.java
+++ src/main/java/io/lindx/boot/App.java
@@ -1,11 +1,19 @@
 package io.lindx.boot;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class App {
   public static void main(String[] args) {
-
     SpringApplication.run(App.class, args);
+  }
+
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplateBuilder().build();
   }
 }
