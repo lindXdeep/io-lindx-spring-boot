20081dc002bd34c71f7e6aef85ae45a5f3cc756f
lindx <linder.igor@gmail.com>
2021-04-13 08:54:39 +0300

    Spring Boot - Tomcat Deployment

--- src/main/java/io/lindx/boot/App.java
+++ src/main/java/io/lindx/boot/App.java
@@ -2,18 +2,17 @@ package io.lindx.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
   }
 
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplateBuilder().build();
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(App.class);
   }
 }
--- src/main/java/io/lindx/boot/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package io.lindx.boot.service;
-
-import io.lindx.boot.model.User;
-
-public interface UserService {
-  User getUser(final Long id);
-}
