e562b13f2cb49c2cc3774dcf5e0402aa175fd4c1
lindx <linder.igor@gmail.com>
2021-04-14 05:18:01 +0300

    add model and controller

--- src/main/java/io/lindx/boot/controllers/MainController.java
+++ src/main/java/io/lindx/boot/controllers/MainController.java
@@ -1,13 +1,27 @@
 package io.lindx.boot.controllers;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.lindx.boot.models.User;
 
 @RestController
 public class MainController {
 
-  @RequestMapping("/hello")
-  public String hello() {
-    return "hello from rest";
+  @GetMapping("/user")
+  public List<User> hello() {
+    return Collections.singletonList(
+      new User(){
+        {
+          setId(1L);
+          setName("User");
+        }
+      }
+    );
   }
 }
