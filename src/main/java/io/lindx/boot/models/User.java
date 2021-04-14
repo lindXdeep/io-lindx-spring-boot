ee9862874aa0b7aa75a15cbfae42d03b335bca13
lindx <linder.igor@gmail.com>
2021-04-14 05:21:11 +0300

    package io.lindx.boot.models;

--- src/main/java/io/lindx/boot/models/User.java
+++ src/main/java/io/lindx/boot/models/User.java
@@ -1,13 +1,16 @@
 package io.lindx.boot.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class User {
   private Long id;
+
+  @JsonProperty("user_name")
   private String name;
 }
